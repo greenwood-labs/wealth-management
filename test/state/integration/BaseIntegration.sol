f373d83a405913bce2ec5498e25df04223262720
Alec DiFederico <alecdifederico@gmail.com>
2022-10-20 10:50:23 -0400

    added console logs for integration test setup

--- test/state/integration/BaseIntegration.sol
+++ test/state/integration/BaseIntegration.sol
@@ -13,6 +13,35 @@ contract BaseIntegration is BaseMultisigFactory, BaseMockPricer {
     function setUp() public virtual override(BaseMultisigFactory, BaseMockPricer) {
         super.setUp();
 
+        console.log("");
+        console.log("##############################################");
+        console.log("###        GREENWOOD MULTISIG SETUP        ###");
+        console.log("##############################################");
+        console.log("");
+        console.log("Gnosis Safe Address:           ", address(safe));
+        console.log("Greenwood Module Address:      ", address(module));
+        console.log("Greenwood Guard Address:       ", address(guard));
+        console.log("Module Proxy Factory Address:  ", address(moduleFactory));
+        console.log("Multisig Factory Address:      ", address(multisigFactory));
+
+        console.log("");
+        console.log("##############################################");
+        console.log("###         GREENWOOD VAULT SETUP          ###");
+        console.log("##############################################");
+        console.log("");
+        console.log("Vault Factory Address:                 ", address(vaultFactory));
+        console.log("Greenwood Vault Address:               ", address(strategy));
+        console.log("Opyn Buffered Note Strategy Address:   ", address(vault));
+
+        console.log("");
+        console.log("##############################################");
+        console.log("###     INTEGRATION TEST INITIAL STATE     ###");
+        console.log("##############################################");
+        console.log("");
+        console.log("Client Address:             ", address(client0));
+        console.log("Counterparty Address:       ", address(counterparty));
+        console.log("Gnosis Safe WETH balance:   ", weth.balanceOf(address(safe)) / 1e18, "WETH");
+    
         labelAddresses();
     }
 }
